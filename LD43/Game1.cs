72f681b5b8fbda60c9a193953cb38f7331ff0932
Fusionnist <antoine.chartier@gmail.com>
2018-11-25 17:37:14 +0100

    a

--- LD43/Game1.cs
+++ LD43/Game1.cs
@@ -2,82 +2,249 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using MonoGame.FZT;
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Data;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Input;
+using MonoGame.FZT.Physics;
+using MonoGame.FZT.Sound;
+using MonoGame.FZT.UI;
+using MonoGame.FZT.XML;
+
+using System;
+using System.Threading;using System.Collections.Generic;
+using System.Xml.Linq;
+
+
 namespace LD43
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+    enum GameState { Game, Menu }
+    enum GameSubState { Game, Main }
+
     public class Game1 : Game
     {
+        //Utility
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        SceneCollection scenes;
+        CustomEntityBuilder eBuilder;
+
+        //Objects
+        InputProfile ipp;
+
+        //Data
+        Point wDims, vDims;
+        GameState currentState, nextState;
+        GameSubState currentSubState, nextSubState;
+        bool switchedState;
         
         public Game1()
         {
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            wDims = new Point(1920 / 3 * 2, 1080 / 3 * 2);
+            vDims = new Point(640, 360);
+
+            //set dat up
+            graphics.PreferredBackBufferWidth = wDims.X;
+            graphics.PreferredBackBufferHeight = wDims.Y;
+            Window.IsBorderless = true;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
-        }
+            currentState = GameState.Menu;
+            currentSubState = GameSubState.Main;
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+            PhysicsManager.CreateWorld();
+            PhysicsManager.SetUnitRatio(64);
+            PhysicsManager.SetupDebugview(GraphicsDevice, Content);
+        }
+        void CreateScenes()
+        {
+            scenes = new SceneCollection();
+
+            //game scene
+            scenes.scenes.Add(new Scene(new RenderTarget2D(GraphicsDevice, vDims.X, vDims.Y), "game"));
+            //menu scene
+            scenes.scenes.Add(new Scene(new RenderTarget2D(GraphicsDevice, vDims.X, vDims.Y), "menu"));
+            //overlay
+            scenes.scenes.Add(new Scene(new RenderTarget2D(GraphicsDevice, vDims.X, vDims.Y), "overlay"));
+            //base
+            scenes.scenes.Add(new Scene(new RenderTarget2D(GraphicsDevice, vDims.X, vDims.Y), new Rectangle(0, 0, vDims.X, vDims.Y), new Rectangle(0, 0, wDims.X, wDims.Y), "base"));
+
+        } //explicit name
+        void CreateGroups()
+        {
+
+        } //entity groups
+        void CreateInputProfile()
+        {
+            ipp = new InputProfile(new List<KeyManager>()
+            {
+                new KeyManager(Keys.Left, "left"),
+                new KeyManager(Keys.Right, "right"),
+                new KeyManager(Keys.Up, "up"),
+                new KeyManager(Keys.Down, "down")
+            });
+        } //check whatever is plugged in to create input settings
+        void CreateNewGame()
+        {
+
+        } //create whatever's needed to start a new game
+
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            ElementCollection.ReadDocument(XDocument.Load("Content/Xml/Registry.xml"));
+            SpriteSheetCollection.LoadFromElementCollection(Content);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
+            ipp.Update(Keyboard.GetState(), GamePad.GetState(0));
+            float es = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            UpdateSwitch(es);
             base.Update(gameTime);
         }
+        void UpdateState()
+        {
+            if (switchedState)
+            {
+                currentState = nextState;
+                currentSubState = nextSubState;
+            }
+        } //set the next state and substate to the current one at the start of the update
+        void UpdateSwitch(float es_)
+        {
+            switch (currentState)
+            {
+                case GameState.Game:
+                    switch (currentSubState)
+                    {
+                        case GameSubState.Game: //GAME-GAME
+                            break;
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+                        case GameSubState.Main: //GAME-MAIN
+                            break;
+                    }
+                    break;
+                case GameState.Menu:
+                    switch (currentSubState)
+                    {
+                        case GameSubState.Game: //MENU-GAME
+                            break;
+
+                        case GameSubState.Main: //MENU-MAIN
+                            break;
+                    }
+                    break;
+            }
+        } //update things based on game state
+        void UpdateGame(float es_)
+        {
+
+        } //update the movey things
+        void UpdateMenu(float es_)
+        {
+
+        } //update the clicky things
+        void ToggleState(GameState newState_)
+        {
+
+        } //set new nextstate and data based on old and new state
+        void ToggleSubState(GameSubState newSubState_)
+        {
+
+        } //set new nextsubstate and data based on old and new sub state
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            DrawScenes();
             base.Draw(gameTime);
-        }
+        } //draw main scene to null target
+        void DrawSwitch()
+        {
+            switch (currentState)
+            {
+                case GameState.Game:
+                    switch (currentSubState)
+                    {
+                        case GameSubState.Game:
+                            break;
+
+                        case GameSubState.Main:
+                            break;
+                    }
+                    break;
+                case GameState.Menu:
+                    switch (currentSubState)
+                    {
+                        case GameSubState.Game:
+                            break;
+
+                        case GameSubState.Main:
+                            break;
+                    }
+                    break;
+            }
+        } //draw sub scenes to main scene based on states
+        void DrawScenes() 
+        {
+            DrawGame();
+            DrawMenu();
+            DrawOverlay();
+        } //draw what should be on each sub scene
+        void DrawGame() 
+        {
+            scenes.SelectScene("game");
+            scenes.SetupScene(spriteBatch, GraphicsDevice);
+
+            //DRAW
+
+            spriteBatch.End();
+        } //draw to game scene
+        void DrawMenu() 
+        {
+            scenes.SelectScene("menu");
+            scenes.SetupScene(spriteBatch, GraphicsDevice);
+
+            //DRAW
+
+            spriteBatch.End();
+        } //draw to menu scene
+        void DrawOverlay() 
+        {
+            scenes.SelectScene("overlay");
+            scenes.SetupScene(spriteBatch, GraphicsDevice);
+
+            //DRAW
+
+            spriteBatch.End();
+        } //draw to overlay scene
+        void DrawPhysicsDebug()
+        {
+            spriteBatch.Begin();
+
+            Matrix projection = Matrix.CreateScale(1f) * Matrix.CreateOrthographicOffCenter(0, vDims.X / 64f, vDims.Y / 64f, 0, 0, 1) * Matrix.CreateTranslation(new Vector3(0, 0, 0));
+
+            PhysicsManager.DrawDebugview(projection);
+
+            spriteBatch.End();
+        } //DEBUG
     }
 }
---
+++ LD43/Utility Classes/CustomEntityBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Data;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Physics;
+
+namespace LD43
+{
+    public class CustomEntityBuilder : EntityBuilder
+    {
+        public CustomEntityBuilder() { }
+        public override Entity CreateEntity(string type_, DrawerCollection dc_, PositionManager pos_, List<Property> props_, string name_)
+        {
+            //Create the default entity
+            if (type_ == "entity")
+            {
+                return new Entity(dc_, pos_, props_, name_, type_);
+            }
+
+            return base.CreateEntity(type_, dc_, pos_, props_, name_);
+        }
+    }
+}
