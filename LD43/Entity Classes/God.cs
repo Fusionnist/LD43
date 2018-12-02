cc74479cbfe554b7f3ebab04366427446d911e25
Fusionnist <antoine.chartier@gmail.com>
2018-12-02 13:21:46 +0100

    a

---
+++ LD43/Entity Classes/God.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.FZT.Assets;
+using MonoGame.FZT.Drawing;
+using MonoGame.FZT.Physics;
+using Microsoft.Xna.Framework;
+
+namespace LD43.Entity_Classes
+{
+    enum GodState { Idle, Attacking }
+    enum GodMood { Ok, Ungry, Pissed, VladimirPutin }
+    public class God : Entity
+    {
+        GodState state;
+        GodMood mod;
+
+        public God(DrawerCollection textures_, PositionManager pos_, List<Property> properties_, string name_, string type_) : base(textures_, pos_, properties_, name_, type_)
+        {
+
+        }
+
+        public void Attack()
+        {
+            textures.GetTex("attack").Reset();
+        }
+
+        public override void Update(float elapsedTime_)
+        {
+            base.Update(elapsedTime_);
+        }
+
+        public override void Draw(SpriteBatch sb_, bool flipH_ = false, bool flipV_ = false, float angle_ = 0)
+        {
+            if(state == GodState.Attacking)
+            {
+                textures.GetTex("attack").Draw(sb_, Vector2.Zero);
+            }
+            base.Draw(sb_, flipH_, flipV_, angle_);
+        }
+    }
+}
